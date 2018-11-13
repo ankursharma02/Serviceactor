e5c12a92a8c0486cc14a7debe20423b7ece766b0
ankursharma02 <ankurs083@outlook.com>
2018-11-13 20:53:35 +0530

    commit

---
+++ MyApplication/ActorClient/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using HelloWord.Interfaces;
+
+
+namespace ActorClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IHelloWord actor = ActorProxy.Create<IHelloWord>(ActorId.CreateRandom(), new Uri("fabric:/MyApplication/HelloWordActorService"));
+            Task<string> retval = actor.GetHelloWordAsync();
+            Console.Write(retval.Result);
+            Console.ReadLine();
+        }
+    }
+}
